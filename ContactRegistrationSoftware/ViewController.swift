10bcba6f2c04191c336625c2d37bcdc8f8cee3a3
Josafat <josamtz@outlook.com>
2022-07-09 01:10:09 -0500

    Initial Commit

---
+++ ContactRegistrationSoftware/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactRegistrationSoftware
+//
+//  Created by Josafat Martínez  on 09/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
